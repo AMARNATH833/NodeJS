91b6e807d203904340518630bc27716098d3deb4
AMARNATH833 <amarnath0862>
2023-03-07 14:09:39 +0530

    NodeJS

---
+++ logging.js
@@ -0,0 +1,10 @@
+// Logging in node
+
+number=500;
+
+if(number<100){
+    console.log("Yes Given number is lesser than 100")
+}
+else{
+    console.error("Sorry Given number is greater than 100")
+}
--- process.js
+++ process.js
@@ -1,5 +1,13 @@
 // import process from 'node:process';
 
+import process from 'node:process';
+
+process.on('beforeExit',(code)=>{
+  console.log('Process beforeExit event with code ' , code);
+})
+
 process.on('exit', (code) => {
   console.log(`About to exit with code: ${code}`);
 });
+
+console.log("This is will displayed first");
